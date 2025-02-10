cdfd4cbf5a9218060c8e50a546b71918d4862b42
Julien <jruumpol@hotmail.com>
2025-02-10 17:05:08 +0100

    [add] initial layout of login page

---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [FormsModule],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent {
+  username: String = '';
+  password: String = '';
+
+
+  onSubmit() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+  }
+}
