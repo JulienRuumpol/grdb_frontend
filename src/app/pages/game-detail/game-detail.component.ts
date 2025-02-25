a7f6ef83be104b80482232807a84f6f0f40c7962
Julien <jruumpol@hotmail.com>
2025-02-25 14:45:18 +0100

    [add] game detail page

--- src/app/pages/game-detail/game-detail.component.ts
+++ src/app/pages/game-detail/game-detail.component.ts
@@ -1,11 +1,118 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { GameService } from '../../services/game.service';
+import { ActivatedRoute } from '@angular/router';
+import { GameDto } from '../../models/dto/game.dto';
+import { MatIcon } from '@angular/material/icon';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatSpinner } from '@angular/material/progress-spinner';
+import {
+  MatSnackBar,
+  MatSnackBarAction,
+  MatSnackBarActions,
+  MatSnackBarLabel,
+  MatSnackBarRef,
+} from '@angular/material/snack-bar';
+import { SnackbarComponent } from '../../components/snackbar/snackbar.component';
+
 
 @Component({
   selector: 'app-game-detail',
-  imports: [],
+  imports: [
+    MatIcon,
+    ReactiveFormsModule,
+    MatSpinner
+  ],
   templateUrl: './game-detail.component.html',
   styleUrl: './game-detail.component.css'
 })
-export class GameDetailComponent {
+export class GameDetailComponent implements OnInit {
 
+  isSaving: Boolean = false;
+  private _snackBar = inject(MatSnackBar);
+
+
+  game: GameDto = {
+    id: 0,
+    name: "",
+    description: ""
+  }
+
+  gameForm = new FormGroup({
+    name: new FormControl<String>('', [
+      Validators.required
+    ]),
+    description: new FormControl<String>('', [
+      Validators.required
+    ])
+  })
+
+  constructor(private gameService: GameService, private route: ActivatedRoute, public snackBar: MatSnackBar) { }
+
+
+
+  ngOnInit(): void {
+    let gameId: number = 0
+    this.route.params.subscribe(param => {
+      gameId = param['id']
+    })
+
+    console.log('we init for id ' + gameId)
+
+    this.gameService.getGameDetail(gameId).subscribe({
+      next: (v) => {
+        this.game = v;
+        console.log("game " + JSON.stringify(this.game))
+
+        this.gameForm.controls.name.setValue(this.game.name)
+        this.gameForm.controls.description.setValue(this.game.description)
+
+      },
+      error: (e) => {
+        console.log('error at ' + JSON.stringify(e))
+        console.log('error ' + e)
+      },
+      complete: () => {
+      }
+    })
+  }
+
+  saveGame() {
+    let newGame = {
+      id: this.game.id,
+      name: this.gameForm.controls.name.value || "",
+      description: this.gameForm.controls.description.value || ""
+    }
+    this.setIsSaving(true)
+
+
+
+
+    console.log('data of the new game is ' + JSON.stringify(this.game))
+    console.log('newgame ' + JSON.stringify(newGame))
+    this.gameService.updateGame(newGame).subscribe({
+      next: (v) => {
+        this.game = v
+        this.setIsSaving(false)
+        this.openSaveSuccesSnackbar()
+      },
+      error: (e) => {
+        console.log('error at ' + JSON.stringify(e))
+      },
+      complete: () => {
+        this.setIsSaving(false)
+
+      }
+    })
+  }
+
+
+  openSaveSuccesSnackbar() {
+    this._snackBar.openFromComponent(SnackbarComponent, {
+      duration: 5000
+    })
+  }
+
+  setIsSaving(value: Boolean) {
+    this.isSaving = value
+  }
 }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { GameDto } from '../models/dto/game.dto';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +10,19 @@ export class GameService {
 
   constructor(private http: HttpClient) { }
 
+  private appurl = 'http://localhost:8080/game'
 
 
-  getGameDetail(gameId: number) {
-    //todo add game detail call 
 
+  getGameDetail(gameId: number): Observable<any> {
+
+
+    return this.http.get(this.appurl + '/' + gameId)
   }
+
+  updateGame(game: GameDto): Observable<GameDto> {
+    return this.http.put<GameDto>(this.appurl + "/", game)
+  }
+
 }
+
