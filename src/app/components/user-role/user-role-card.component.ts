182679aa539eec766bd071d5342047e199f1f398
Julien <jruumpol@hotmail.com>
2025-04-30 12:03:04 +0200

    [add] update user role

--- src/app/components/user-role/user-role-card.component.ts
+++ src/app/components/user-role/user-role-card.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input, input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatCard } from '@angular/material/card';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { Role } from '../../models/role.model';
 import { User } from '../../models/user.model';
 import { MatOption } from '@angular/material/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatSpinner } from '@angular/material/progress-spinner';
+import { MatSelect } from '@angular/material/select';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-role-card',
@@ -19,7 +21,9 @@ import { MatSpinner } from '@angular/material/progress-spinner';
     MatOption,
     MatIcon,
     MatFormField,
-    MatSpinner
+    MatSpinner,
+    MatSelect,
+    MatFormFieldModule
 
   ],
   templateUrl: './user-role-card.component.html',
@@ -39,12 +43,33 @@ export class UserRoleCardComponent implements OnInit {
 
   roleControl: FormControl = new FormControl<Role>({ id: 0, name: "Basic" })
 
+  constructor(private userService: UserService) { }
+
   ngOnInit(): void {
-    console.log('user is ' + JSON.stringify(this.user))
-    console.log('role is  ' + JSON.stringify(this.roles))
+    this.roleControl.patchValue({
+      id: this.user.role.id,
+      name: this.user.role.name
+    })
   }
 
-  updateUserRole(userId: Number) {
+  updateUserRole() {
 
+    if (this.roleControl.valid) {
+
+      this.isSaving = true
+      this.userService.updateRole(this.user.id, this.roleControl.value).subscribe({
+        next: (v: User) => {
+          this.user.role = v.role
+        },
+        error: (e) => {
+          console.log('error updating role at ' + JSON.stringify(e))
+
+        },
+        complete: () => {
+          this.isSaving = false
+        }
+      })
+
+    }
   }
 }
