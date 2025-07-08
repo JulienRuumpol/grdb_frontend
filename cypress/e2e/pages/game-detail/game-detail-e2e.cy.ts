0b8c52e2df2510a77103eeff88aa1eca87a13211
Julien <jruumpol@hotmail.com>
2025-07-08 12:20:30 +0200

    [add] app images

--- cypress/e2e/pages/game-detail/game-detail-e2e.cy.ts
+++ cypress/e2e/pages/game-detail/game-detail-e2e.cy.ts
@@ -12,18 +12,38 @@ describe('game-detail-page testing', () => {
         cy.url().should('eq', 'http://localhost:4200/game/1')
 
     })
-    it('place a review', () => {
-        if (cy.get('[data-cy="addReviewCard"').should('exist')
-        ) {
-            //     cy.get('[data-cy="reviewInput"').type("this a review made by cypress")
-            // cy.get('[data-cy="addReviewButton"').click()
-            //         cy.get('[data-cy="reviewCard"').
+    it('place a review if possible', () => {
+        var hasReview: Boolean = false;
+        // somehow it still goes through here. Perhaps it's best to use this as a boolean
+        if (cy.get('[data-cy="addReviewCard"').should('exist')) {
+            cy.get('[data-cy="addReviewInput"').type("this a review made by cypress")
+            cy.get('[data-cy="addReviewButton"').click()
+
+            cy.get('[data-cy="reviewCard"').each(($el, index, $list) => {
+                cy.wrap($el).find('[data-cy="reviewInput"').invoke('val')
+                    .then(text => {
+                        const reviewText = text;
+
+                        // check if review text is equal to expected text
+                        if (!hasReview && reviewText === "a lot of action which requires good gameplay") {
+                            hasReview = true
+
+                        }
+                    });
+
+                // check boolean value to be true
+                cy.then(() => {
+                    expect(hasReview).to.be.true;
+                });
+            })
 
         }
+    })
 
+
+    it('checks for exisiting review', () => {
         var hasReview: Boolean = false;
 
-        // code to go through each reviewCard and check element if it has the required text that is used testing
         cy.get('[data-cy="reviewCard"').each(($el, index, $list) => {
             cy.wrap($el).find('[data-cy="reviewInput"').invoke('val')
                 .then(text => {
@@ -32,17 +52,57 @@ describe('game-detail-page testing', () => {
                     // check if review text is equal to expected text
                     if (!hasReview && reviewText === "a lot of action which requires good gameplay") {
                         hasReview = true
-
                     }
                 });
 
 
         })
-
         // check boolean value to be true
         cy.then(() => {
             expect(hasReview).to.be.true;
         });
+
     })
+
+    it('removes the self created review', () => {
+
+        cy.get('[data-cy="reviewCard"').each(($el, index, $list) => {
+            cy.wrap($el).find('[data-cy="reviewInput"').invoke('val')
+                .then(text => {
+                    const reviewText = text;
+
+                    // check if review text is equal to expected text
+                    if (reviewText === "this a review made by cypress") {
+                        cy.wrap($el).get('[data-cy="deleteReviewButton"').click
+                        console.log("we found review")
+                    }
+                });
+
+
+
+        })
+        var hasReview: Boolean = false;
+
+        cy.get('[data-cy="reviewCard"').each(($el, index, $list) => {
+            cy.wrap($el).find('[data-cy="reviewInput"').invoke('val')
+                .then(text => {
+                    const reviewText = text;
+
+                    // check if review text is equal to expected text
+                    if (!hasReview && reviewText === "a lot of action which requires good gameplay") {
+                        hasReview = true
+                    }
+                });
+
+
+        })
+        // check boolean value to be true
+        cy.then(() => {
+            expect(hasReview).to.be.false;
+        });
+
+
+    })
+
 
 })
